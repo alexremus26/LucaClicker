fe41d08a23c33ea943e3d3e31e8645962f329262
Stoici <alexceva26@gmail.com>
2025-12-09 04:34:36 +0200

    Adaugare clasa derivata beverage - fara implementare in interfata

---
+++ src/Beverage.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Stoici on 12/8/2025.
+//
+
+#include "Beverage.h"
---
+++ src/Beverage.h
@@ -0,0 +1,13 @@
+//
+// Created by Stoici on 12/8/2025.
+//
+
+#ifndef OOP_BEVERAGE_H
+#define OOP_BEVERAGE_H
+
+
+class Beverage {
+};
+
+
+#endif //OOP_BEVERAGE_H
